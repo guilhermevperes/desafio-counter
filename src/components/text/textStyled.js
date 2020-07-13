bec2c756e01a414891f8e574a5c41348109f2bae
Guilherme Peres <guilherme.vperes@gmail.com>
2020-07-13 18:27:39 -0300

    Configuracao de fontes

--- src/components/text/textStyled.js
+++ src/components/text/textStyled.js
@@ -2,5 +2,6 @@ import styled from 'styled-components/native'
 
 export const Text = styled.Text`
     font-size: 30px;
-    color: ${props => props.color}
+    color: ${props => props.color};
+    font-family: 'Montserrat-Bold'
 `
